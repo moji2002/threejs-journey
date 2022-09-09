2c0749776beaff9ef542174e510102dc3125e7e8
Mojtaba Beheshti <moji2002@gmail.com>
2022-09-09 21:34:13 +0430

    render mesh

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const canvas = document.querySelector('canvas')
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-  color: "red",
+  color: 0xff6655,
 });
 
 const cubeMesh = new THREE.Mesh(geometry, material);
 scene.add(cubeMesh);
 
-const camera = new THREE.PerspectiveCamera()
+const FIELD_OF_VIEW = 50; //degrees
+const SIZES = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-const FIELD_OF_VIEW = 50 //degrees
-scene.add(camera)
+const ASPECT_RATIO = SIZES.width / SIZES.height;
+const camera = new THREE.PerspectiveCamera(FIELD_OF_VIEW, ASPECT_RATIO);
+camera.position.y = 0
+camera.position.x = 0
+camera.position.z = 3
+scene.add(camera);
+
+const canvas = document.querySelector("#element");
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+
+renderer.setSize(SIZES.width,SIZES.height)
+renderer.render(scene,camera)
