412aea0e261e576f7ea0b168dd238ab21954088a
Mojtaba Beheshti <moji2002@gmail.com>
2022-09-10 00:40:04 +0430

    object position

--- src/script.js
+++ src/script.js
@@ -1,32 +1,32 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
 
-const canvas = document.querySelector('#webgl')
-
-const scene = new THREE.Scene()
-
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x224433 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
+const canvas = document.querySelector("#webgl");
 const SIZES = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x224433 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0.5, 1, 0.1);
 
 const ASPECT_RATIO = SIZES.width / SIZES.height;
 const FIELD_OF_VIEW = 50; //degrees
+const camera = new THREE.PerspectiveCamera(FIELD_OF_VIEW, ASPECT_RATIO);
+// camera.position.x = .5;
+// camera.position.y = .5;
+camera.position.z = 3;
 
-const camera = new THREE.PerspectiveCamera(FIELD_OF_VIEW, ASPECT_RATIO)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 3
-scene.add(camera)
+camera.lookAt(mesh.position)
 
-const renderer = new THREE.WebGLRenderer({
-    canvas
-})
+const objects = [camera, mesh, new THREE.AxesHelper()];
+for (const object of objects) scene.add(object);
 
-renderer.setSize(SIZES.width, SIZES.height)
-renderer.render(scene, camera)
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(SIZES.width, SIZES.height);
+renderer.render(scene, camera);
